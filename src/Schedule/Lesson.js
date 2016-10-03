eb264f0c8930fcdf2ea3878836f69192f46180fb
shyr1punk <shyr1punk@gmail.com>
2016-10-03 23:10:08 +0300

    Lessons colours

--- src/Schedule/Lesson.js
+++ src/Schedule/Lesson.js
@@ -1,12 +1,26 @@
 import React, { Component, PropTypes } from 'react';
 
 import { TableRow, TableRowColumn } from 'material-ui/Table';
+import './lesson.css';
 
 export default class Lesson extends Component {
   render() {
-    console.log(this.props);
+    let cl = '';
+    switch (this.props.lesson_type) {
+      case 'Лекция':
+        cl = 'lesson--lection';
+        break;
+      case 'Пр.Зан.':
+        cl = 'lesson--prac';
+        break;
+      case 'Лаб.раб.':
+        cl = 'lesson--lab';
+        break;
+      default:
+        cl = '';
+    }
     return (
-      <TableRow>
+      <TableRow className={cl}>
         <TableRowColumn>{this.props.number}</TableRowColumn>
         <TableRowColumn style={{whiteSpace: 'initial'}}>{this.props.subject}</TableRowColumn>
         <TableRowColumn>{this.props.auditory}</TableRowColumn>
